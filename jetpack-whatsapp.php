75046cbb66c78ec603a8bdc82ba5c6ef763fd3c0
Valerio Souza <valeriosza@gmail.com>
2015-04-09 21:25:08 -0300

    beta 16

--- jetpack-whatsapp.php
+++ jetpack-whatsapp.php
@@ -3,7 +3,7 @@
  * Plugin Name: Jetpack WhatsApp
  * Plugin URI: http://wordpress.org/plugins/*******
  * Description: Add Jetpack Whatsapp
- * Version: 0.0.15
+ * Version: 0.0.16
  * Author: Valerio Souza
  * Author URI: http://www.valeriosouza.com.br
  * License: AGPLv3 or later
