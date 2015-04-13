e7d747d31c0d70dc98c805b3c6decbb01eee9d0d
Valerio Souza <valeriosza@gmail.com>
2015-04-13 16:28:44 -0300

    update url base

--- class.whatsapp-source.php
+++ class.whatsapp-source.php
@@ -59,7 +59,7 @@ class jetwhats_Share_WhatsApp extends Sharing_Source {
 	}
 
 	function process_request( $post, array $post_data ) {
-		$whatsapp_url = 'whatsapp://send?text='.rawurlencode(__('Read this','jetpack-whatsapp').':%20'.$this->get_share_title( $post->ID ).'%20-%20'.$this->get_share_url( $post->ID ) ).'';
+		$whatsapp_url = 'whatsapp://send?text='.rawurlencode(__('Read this','jetpack-whatsapp').': '.$this->get_share_title( $post->ID ).' - '.$this->get_share_url( $post->ID ) ).'';
 
 		// Record stats
 		parent::process_request( $post, $post_data );
