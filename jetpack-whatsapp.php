6b56fb124b591cd7ca1395ad8a741f581364aead
valeriosouza <valeriosza@gmail.com>
2015-10-05 21:12:00 -0300

    update version

--- jetpack-whatsapp.php
+++ jetpack-whatsapp.php
@@ -3,7 +3,7 @@
  * Plugin Name: WhatsApp Sharing Button for Jetpack
  * Plugin URI: http://valeriosouza.com.br/portfolio/whatsapp-sharing-button-for-jetpack/?utm_source=plugin&utm_medium=plugin-url&utm_campaign=jetpack-whatsapp
  * Description: Add WhatsApp button to Jetpack Sharing
- * Version: 1.1.2
+ * Version: 1.1.3
  * Author: Valerio Souza, WordLab Academy
  * Author URI: http://www.valeriosouza.com.br
  * License: GPLv3 or later
@@ -48,7 +48,7 @@ function jw_dependencies_notice() {
 define( 'jetwhats__PLUGIN_DIR',  plugin_dir_path( __FILE__ ) );
 define( 'jetwhats__PLUGIN_URL',  plugin_dir_url( __FILE__ ) );
 define( 'jetwhats__PLUGIN_FILE', __FILE__ );
-define( 'jetwhats__VERSION',     '1.1.2' );
+define( 'jetwhats__VERSION',     '1.1.3' );
 
 add_action( 'init', array( 'Jetpack_Whatsapp_Pack', 'init' ) );
 
@@ -71,10 +71,11 @@ class Jetpack_Whatsapp_Pack {
 	}
 
 	private function __construct() {
-		add_action( 'wp_enqueue_scripts',    array( &$this, 'register_assets' ) );
-		add_action( 'admin_enqueue_scripts', array( &$this, 'admin_menu_assets' ) );
-		add_action( 'admin_notices', 		 array( &$this, 'plugin_donate_notice' ) );
-		add_action( 'admin_init', 			 array( &$this, 'plugin_donate_ignore' ) );
+		add_action( 'wp_enqueue_scripts',    	array( &$this, 'register_assets' ) );
+		add_action( 'admin_enqueue_scripts', 	array( &$this, 'admin_menu_assets' ) );
+		add_action( 'admin_notices', 		 	array( &$this, 'plugin_donate_notice' ) );
+		add_action( 'admin_init', 			 	array( &$this, 'plugin_donate_ignore' ) );
+		register_deactivation_hook( __FILE__,	array( &$this, 'update' ));
 
 		if( did_action('plugins_loaded') ) {
 			$this->require_services();
@@ -83,6 +84,12 @@ class Jetpack_Whatsapp_Pack {
 		}
 		add_filter( 'plugin_row_meta', array( &$this, 'plugin_row_donate' ), 10, 2 );
 		add_filter( 'plugin_row_meta', array( &$this, 'plugin_row_more' ), 10, 2 );
+	}
+
+	static function update() {
+		global $current_user;
+	    $user_id = $current_user->ID;
+		delete_user_meta($user_id, 'whatsapp_ignore_notice');
 	}
 
 	function register_assets() {
@@ -145,7 +152,7 @@ class Jetpack_Whatsapp_Pack {
 	        /* Check that the user hasn't already clicked to ignore the message */
 		if ( ! get_user_meta($user_id, 'whatsapp_ignore_notice') ) {
 	        echo '<div class="updated"><p>';
-	        printf(__('Like the plugin to share the WhatsApp Sharing Button for Jetpack? Develop free plugins takes work! Be my boss and make a <a target="_blank" href="%1$s">donation of any amount</a>. | <a href="%2$s">Hide Notice</a>'), 'http://wordlab.com.br/donate/?utm_source=plugin&utm_medium=donate-notice&utm_campaign=jetpack-whatsapp', '?whatsapp_nag_ignore=0');
+	        printf('%s<a target="_blank" href="%s">%s</a>. | <a href="%s">%s</a>',__('Like the plugin to share the WhatsApp Sharing Button for Jetpack? Develop free plugins takes work! Be my boss and make a ', 'jetpack-whatsapp'), 'http://wordlab.com.br/donate/?utm_source=plugin&utm_medium=donate-notice&utm_campaign=jetpack-whatsapp', __('donation of any amount', 'jetpack-whatsapp'), '?whatsapp_nag_ignore=0',__('This plugin does not deserve a donation', 'jetpack-whatsapp'));
 	        echo "</p></div>";
 		}
 	}
--- readme.txt
+++ readme.txt
@@ -4,7 +4,7 @@ Donate link: http://wordlab.com.br/donate/
 Tags: Jetpack, social, sharing, share, SNS,WhatsApp, whatsapp, WHATSAPP, Jetpack Sharing, Button WhatsApp, WhatsApp Button for Jetpack Sharing, WhatsApp Sharing Button for Jetpack
 Requires at least: 3.8
 Tested up to: 4.3.1
-Stable tag: 1.1.2
+Stable tag: 1.1.3
 License: GPLv3 or later
 License URI: http://www.gnu.org/licenses/gpl.txt
 
@@ -65,6 +65,10 @@ For some reason, we do not know or WhatsApp application, or Windows Phone, the b
 
 == Changelog ==
 
+= 1.1.2 - 06/10/2015 =
+
+* Fixed bugs in translate
+
 = 1.1.2 - 05/10/2015 =
 
 * Fixed bugs
@@ -95,6 +99,10 @@ For some reason, we do not know or WhatsApp application, or Windows Phone, the b
 * Initial Beta Release
 
 == Upgrade Notice ==
+
+= 1.1.2 - 06/10/2015 =
+
+* Fixed bugs in translate
 
 = 1.1.2 - 05/10/2015 =
 
